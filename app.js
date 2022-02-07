b2225ff7b3b931dcb05b180d95a4dc37d60e9990
ChengZhen97 <90570127+ChengZhen97@users.noreply.github.com>
2022-02-06 17:32:31 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const request = require('request');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const http = require('http');
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/v1/weather', (req, res) => {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=corvallis&appid=a99f1dbe7d7e7d5b6ce85970a31da042'
+  request(url, (error, response, body)=>{
+    console.log(body);
+    let json = JSON.parse(body);
+    res.json(json);
+  });
+
+})
+
+app.get('/v1/hello', (req, res) => {
+  const sayHello = "Hello world";
+  res.json({sayHello});
+})
+
+app.post('/v1/auth', (req, res) =>{
+
+  const username = req.body.username;
+  const password = req.body.password;
+  //console.log(username, password);
+     if(username != "usr" || password != "pass"){
+    res.send("not an authorized user");
+  }else{
+    const myToken = "eyJwYXNzd29yZCI6IlBhc3N3b3JkIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTY0NDE5MjE4OCwiZXhwIjoxNjQ0MTk1Nzg4fQ.YP0OKHcljuZf6X9V8tMimfcOrzTc63-ia9rRrgOA0eA"
+    res.json({myToken});
+  } 
+  //res.sendStatus(200);
+  res.json({requestBody : req.body})
+});
+
